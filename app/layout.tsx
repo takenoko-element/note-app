c083e1fab74e660a98ebf8ddbb07c5e24955c78a
Takeshi Fukumoto <takenokofukumoto@gmail.com>
2025-07-27 17:03:08 +0900

    [feature] NJT-39 ノートの編集機能の実装＆Toastの実装 (#3)

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import Providers from '@/components/Providers';
+import { Toaster } from 'sonner';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,8 +19,10 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body className={inter.className}>
-        {/* 2. アプリ全体をProvidersでラップする */}
-        <Providers>{children}</Providers>
+        <Providers>
+          {children}
+          <Toaster richColors />
+        </Providers>
       </body>
     </html>
   );
